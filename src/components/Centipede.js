94cc603c7044e214381b7ba89c6f2915758d149b
Steven <steven.r987@gmail.com>
2021-10-18 13:33:29 -0600

    outcome explanation

--- src/components/Centipede.js
+++ src/components/Centipede.js
@@ -5,6 +5,7 @@ const Centipede = () => {
     const [pot, setPot] = useState(100);
     const [take, setTake] = useState(false);
 
+    //handle bot turn as well as maxiumum turns
     const handlePass = () => {
         setCount(prevCount => prevCount + 1);
         setPot(prevPot => prevPot + 5);
@@ -28,7 +29,20 @@ const Centipede = () => {
             <h3>Count is {count}</h3>
             <button onClick={handlePass}>Pass</button>
             <button onClick={handleTake}>Take</button>
-            {take ? "You took the pot totalling $" + pot + "!": null}
+            {take ?
+            <div className="explanation">
+                <h3>You took the pot totalling ${pot}!</h3> 
+                <p>The subgame perfect equilibrium (a refinement of the nash equilibrium) 
+                for Centipede tells us to take the pot on the first turn,
+                which is established by the process of backwards induction. 
+                For example, if play reaches the last round between player A and B, and
+                it's B's turn, B should take the pot or they will lose. Since B must take
+                the pot that turn, A should take the pot on the second to last turn
+                in order to win before B. Knowing this, B should take the pot on the third
+                to last turn. This reasoning continues backwards through the game tree until
+                one finds that the best action is the first player defecting on the first round.
+                </p>
+            </div> : null }
         </div>
     )
 }
