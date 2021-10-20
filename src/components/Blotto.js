9fc8fddccd5b930e3a73a71b4264a922051f55ea
Steven <steven.r987@gmail.com>
2021-10-20 15:15:17 -0600

    bot strategy for blotto, calculates winner

--- src/components/Blotto.js
+++ src/components/Blotto.js
@@ -6,34 +6,50 @@ import { faRedo } from '@fortawesome/free-solid-svg-icons';
 
 const Blotto = () => {
     const resources = 13;
+    const [winner, setWinner] = useState(false);
+    const [botNumbers, setBotNumbers] = useState(0);
     const [total, setTotal] = useState(0);
-    const [values,set_values] = useState({
+    const [error, setError] = useState(false);
+    const [values,setValues] = useState({
         bf1:1,
         bf2:1,
         bf3:1,
     })
-    
     const [maxValues,setMaxValues] = useState({
         bf1:11,
         bf2:11,
         bf3:11,
     })
+    // For S = 13, choosing (3, 5, 5), (3, 3, 7) and (1, 5, 7) 
+    const permutations = [
+        {bf1: 3, bf2: 5, bf3: 5},
+        {bf1: 5, bf2: 3, bf3: 5},
+        {bf1: 5, bf2: 5, bf3: 3},
+        {bf1: 3, bf2: 7, bf3: 3},
+        {bf1: 7, bf2: 3, bf3: 3},
+        {bf1: 3, bf2: 3, bf3: 7},
+        {bf1: 1, bf2: 5, bf3: 7},
+        {bf1: 5, bf2: 7, bf3: 1},
+        {bf1: 7, bf2: 1, bf3: 5},
+        {bf1: 7, bf2: 5, bf3: 1},
+        {bf1: 1, bf2: 7, bf3: 5},
+        {bf1: 5, bf2: 1, bf3: 7}
+    ];
     
-    const values_handler = (e) => {
+    const valuesHandler = (e) => {
         let name= e.target.name;
         let value= e.target.value;
         const newValues = {
             ...values,
             [name]: value
         }
-        set_values(newValues)
-        calc_total(newValues) 
+        setValues(newValues)
+        calcTotal(newValues) 
     }
     
-    const calc_total = (newValues) => {
+    const calcTotal = (newValues) => {
         const { bf1, bf2, bf3} = newValues;
         const newTotal = parseInt(bf1) + parseInt(bf2) + parseInt(bf3)
-        
         setTotal(newTotal);
 
         //if a non-number is entered it disables input
@@ -41,9 +57,7 @@ const Blotto = () => {
             setMaxValues({bf1: 0, bf2: 0, bf3: 0});
             return;
         }
-    
         const difference = resources-newTotal;
-
         const newMaxValues = {
             ...values,
                 "bf1": parseInt(bf1) + difference,
@@ -55,11 +69,71 @@ const Blotto = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
-        if(total == resources){
+        if(total === resources){
             //allow to proceed
+            setError(false);
+            let botResult = botStrategy();
+            setBotNumbers(botResult); 
+            findResult(botResult);
         } else {
             //ask for total resources to equal 13
+            setError(true);
+        }
+    }
+
+    function roll() {
+        return Math.floor(Math.random() * 11 + 1); //1-5 uniformly
+     }
+
+    function chooseNumbers() {
+        var x = 0, y = 0, z = 0;
+
+        while (x + y + z !== 13) {
+           x = roll();
+           y = roll();
+           z = roll();
+        }
+        return {bf1: x, bf2: y, bf3: z};
+    }
+
+    function choosePermutation() {
+        return permutations[Math.floor(Math.random() * permutations.length)];
+     }
+
+    // For S = 13, choosing (3, 5, 5), (3, 3, 7) and (1, 5, 7) 
+    //with probability 1/3 each can be shown to be the optimal probabilistic strategy.
+
+    //half the time follow optimal probabilistic, other half randomize
+    const botStrategy = () => {
+        var strat;
+        const roll = Math.floor(Math.random() * 10);
+        if(roll <= 5){
+            //optimal
+            strat = choosePermutation();
+        } else {
+            //random
+            strat = chooseNumbers();
+        }
+        return strat;
+    }
+
+    const findResult = (botNums) => {
+        let human = 0, bot = 0;
+        Object.keys(values).forEach( function (key) {
+            if (values[key] > botNums[key]){
+                human += 1;
+            } else if (botNums[key] > values[key]){
+                bot += 1;
+            } else {
+            }
+        });
+
+        if(human > bot){
+            setWinner('You won!');
+        } else if (bot > human) {
+            setWinner('The bot won!');
+        } else {
+            setWinner("It's a tie!");
         }
     }
     
@@ -68,18 +142,27 @@ const Blotto = () => {
         <div className="centipede">
             <h2>Colonel Blotto Game</h2>
             <p>
-                Two players distribute finite resources over several battlefields, with the player who
-                allocated the most resources to a battlefield winning that battlefield. The payoff is
-                equal to the total number of battles won. In this version, players will record three
+                Two players distribute finite resources over several battlefields, with each
+                specific battle decided by whoever allocated more resources. In this version, players will record three
                 positive integers that add up to {resources}. The player who has two numbers
-                higher than the opponents wins the round, other cases are a draw.
+                higher than the opponents wins the round, and if points are tied its a draw.
             </p>
             <form onSubmit={handleSubmit}>
-                <input type="number" id="bf1" value={values["bf1"]} name="bf1" min="1" max={maxValues["bf1"]} onInput={values_handler} />
-                <input type="number"  id="bf2" value={values["bf2"]} name="bf2" min="1" max={maxValues["bf2"]} onInput={values_handler}/>
-                <input type="number" id="bf3" value={values["bf3"]} name="bf3" min="1" max={maxValues["bf3"]} onInput={values_handler}/>
+                <input type="number" id="bf1" value={values["bf1"]} name="bf1" min="1" max={maxValues["bf1"]} onInput={valuesHandler} />
+                <input type="number"  id="bf2" value={values["bf2"]} name="bf2" min="1" max={maxValues["bf2"]} onInput={valuesHandler}/>
+                <input type="number" id="bf3" value={values["bf3"]} name="bf3" min="1" max={maxValues["bf3"]} onInput={valuesHandler}/>
                 <button type="submit">Submit</button>
+                {error ? <p>Enter three numbers totaling {resources}.</p> : null}
             </form>
+            {botNumbers !== 0 ?
+                <div className="input">
+                    <h3>Bot Entered: </h3>
+                    <input type="number" value={botNumbers.bf1} disabled={true} />
+                    <input type="number" value={botNumbers.bf2} disabled={true} /> 
+                    <input type="number" value={botNumbers.bf3} disabled={true} />
+                </div>
+                : null}
+            {winner ? <h3>{winner}</h3> : null}
         </div>
     )
 }
